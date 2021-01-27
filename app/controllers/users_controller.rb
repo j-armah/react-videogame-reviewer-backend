aeb8b68b6e5d0b312836dbaf1b8028420af055b6
februar-y <jarmah241@gmail.com>
2021-01-26 23:36:18 -0500

    adds custom path profile to keep user logged in after refresh

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,18 @@
 class UsersController < ApplicationController
 
-    #before_action :authorize
+    before_action :authorize, only: [:profile]
 
     def index
         users = User.all
         render json: users
     end
 
-    def show 
+    def show
         user = User.find_by(id: params[:id])
         render json: user
+    end
+
+    def profile
+        render json: @user
     end
 end
